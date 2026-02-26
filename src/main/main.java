68855c4bf827207ba48315a9f4891baa205e57f7
lisboa <lisboa.pedrohenrique1@gmail.com>
2026-02-25 21:24:09 -0300

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+
+import model.Cadeira;
+
+public class main {
+
+    public static void main(String[] args) {
+        Cadeira cadeira1 = new Cadeira();
+    }
+
+}
---
+++ src/model/Cadeira.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Cadeira {
+    public int quantidade_pes;
+    public String material;
+    public float altura;
+}
