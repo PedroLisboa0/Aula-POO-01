c22a64d154cdf5c908ee92c802d7bbc69aef4db3
lisboa <lisboa.pedrohenrique1@gmail.com>
2026-03-09 20:14:34 -0300

    feat: adiciona métodos de comportamento

--- src/main/main.java
+++ src/main/main.java
@@ -3,10 +3,21 @@ package main;
 
 import model.Cadeira;
 
-public class main {
+public class SistemaPrincipal {
 
     public static void main(String[] args) {
-        Cadeira cadeira1 = new Cadeira();
-    }
+        // INSTANCIAÇÃO
+        // O comando 'new' aloca memória para um novo objeto.
+        // Criando a primeira cadeira (Objeto 1)
+        Cadeira cadeira1 = new Cadeira("Madeira", 4, 1.0F);
+        System.out.println("Mudar material cadeira 1");
+        cadeira1.mudarMaterial("Estofado"); // Sem erros
 
+        // Criando a segunda cadeira (Objeto 2)
+        Cadeira cadeira2 = new Cadeira("Metal", 5, 1.3F);
+        System.out.println("Mudar material cadeira 2");
+        cadeira2.mudarMaterial(4); // Erro de material inválido
+
+
+    }
 }
--- src/model/Cadeira.java
+++ src/model/Cadeira.java
@@ -4,4 +4,29 @@ public class Cadeira {
     public int quantidade_pes;
     public String material;
     public float altura;
+
+    public Cadeira(String material, int quantidade_pes, float altura) {
+        this.material = material;
+        this.quantidade_pes = quantidade_pes; 
+        this.altura = altura;
+    }
+    public void subirCadeira(float distancia) {
+        // Regra de negócio: O valor da distancia deve ser positivo
+        if (distancia <= 0) {
+            System.out.println("Erro: O valor de distância deve ser maior que zero.");
+            return;
+        }
+        this.altura += distancia;
+        System.out.println("Cadeira erguida! Nova altura: " + this.altura);
+    }
+    public void mudarMaterial(String material) {
+        // Regra de negócio: O material deve ser uma palavra
+        if (!(material instanceof String)) {
+            System.out.println("Erro: O material é inválido.");
+            return;
+        }
+        this.material = material;
+        System.out.println("Material atualizado! Novo material: " + this.material);
+    }
 }
+
